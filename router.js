3a2b82cb8aad64bbfcabd59d26f516611a31e2f9
youssef_selmy <ym79793@gmail.com>
2022-12-16 16:10:06 +0200

    update in html code

---
+++ router.js
@@ -0,0 +1,17 @@
+/*const express = require('express');
+const Message = require('tedious/lib/message');
+const { addDataToLocalStorageFrom, getDataFromLocalStorage, deleteTaskWith } = require('./main');
+const router = express.Router()
+
+router.get('/', getDataFromLocalStorage);
+
+
+router.post('/', addDataToLocalStorageFrom);
+
+
+router.delete('/:taskId', deleteTaskWith);
+
+
+
+
+module.exports = router*/
---
+++ taskserver.js
@@ -0,0 +1,15 @@
+/*const express = require('express');
+const db = require('./dataB');
+const tasksRouter = require('./router');
+
+
+const app = express();
+app.use(express.json()); //to read data before send to body
+app.use('/tasks', tasksRouter);
+
+
+
+
+app.listen(8080, () => {
+    console.log('listing on port 8080');
+});*/
